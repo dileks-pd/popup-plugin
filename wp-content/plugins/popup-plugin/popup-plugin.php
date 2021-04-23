4ec2477874a478cae7d53c32369507f36614b84e
hokka <s.dishli@dileks.ru>
2021-04-23 08:34:16 +0300

    new sql queries update functionality

--- wp-content/plugins/popup-plugin/popup-plugin.php
+++ wp-content/plugins/popup-plugin/popup-plugin.php
@@ -1,53 +1,42 @@
 <?php
 /**
-
   Plugin Name: Dilex-air Plugin
   
   Plugin URI: https://dilex-air/
-
   Description: Popup Plugin is the simple WordPress plugin for handling modals and popups. Take this as a base plugin and modify as per your need.
-
   Version: 1.0
-
   Author: Dilex-air
-
   Author URI: https://github.com/dileks-pd
-
   Text Domain: popup plugin
-
   Contributors: Dilex
-
   License: GPL-2.0+
-
   License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-include 'php-scripts/db-modification.php';
 //  include 'db-connection.php'
 
 function add_popup_form( $title ) {
-    return <<<HTML
+  return <<<HTML
     <html>
     <body>
         <div id="overlay">
           <form id="modal-container" method="post" action="popup-plugin.php">
           <!-- <form id="modal-container" method="post" action="/"> -->
-
-            <label for="input1" name='name_col'>Поле1</label>
-            <input type="text" id="smth">
-            <label for="input2" name='phone_col'>Поле2</label>
-            <input type="text" id="smth"> 
-            <label for="input3" name='email_col'>Поле3</label>
-            <input type="text" id="smth">
+            <label for="input1">Поле1</label>
+            <input type="text" name='name_col' id="smth">
+            <label for="input2">Поле2</label>
+            <input type="text" name='phone_col' id="smth"> 
+            <label for="input3">Поле3</label>
+            <input type="text" name='email_col' id="smth">
             <button type="submit">отправить</button>
           </form>
         </div>
-
     </body>
     </html>
 HTML;
 }
 
+include 'php-scripts/db-modification.php';
 // $value = 'str';
 // echo($value);
 
